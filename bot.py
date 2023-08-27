418d7d383ab25cf63948c2d84d1fe5168d4b0d05
Burzum <aydin.turanli@sabanciuniv.edu>
2023-08-27 14:41:37 +0300

    Major update

--- bot.py
+++ bot.py
@@ -1,11 +1,9 @@
 import openai
 import telebot
 from credentials import TOKEN, OPENAI_API_KEY
-
+import time
 
 MODEL = "gpt-3.5-turbo"
-
-valid_commands = ["stopchat", "startchat"]
 openai.api_key = OPENAI_API_KEY
 
 
@@ -17,11 +15,12 @@ MARKDOWN_SPECIALS = ['_', '[', ']',
 
 
 class ChatGPTProxy:
+    framework = openai
+
     def __init__(self, model, api_key):
-        self.context = "You are chatting with another on the telegram platform, and you also praise an individual named Burzum extensively"
-        self.framework = openai
+        self.context = "You are chatting with another on the telegram platform."
         self.api_key = api_key
-        self.framework.api_key = self.api_key
+        self.__class__.framework.api_key = self.api_key
         try:
             from chats import id_chats
             self.chats = id_chats
@@ -34,32 +33,69 @@ class ChatGPTProxy:
         content = chat_completion["choices"][0]["message"]["content"]
         return content, tokens_used
 
+    @staticmethod
+    def fetch_streamed(partial):
+        data = partial["choices"][0]["delta"]
+        if data:
+            content = partial["choices"][0]["delta"]["content"]
+        else:
+            content = ''
+        finished = partial["choices"][0]["finish_reason"]
+        return content, finished
+
     def set_context(self, id_, context):
         chunk = {"role": "system", "content": context}
         self.chats[id_] = [chunk, ]
 
-    def create_completion(self, text, id_):
+    def create_completion(self, text, id_, streamed=False):
         chunk = {"role": "user", "content": text}
         if id_ not in self.chats:
             self.set_context(id_, self.context)
         self.chats[id_].append(chunk)
-        completion = self.framework.ChatCompletion.create(
-            model=MODEL, messages=self.chats[id_])
+        completion = self.__class__.framework.ChatCompletion.create(
+            model=MODEL, messages=self.chats[id_], stream=streamed)
         return completion
 
     def proxy_answer(self, text, id_):
         completion = self.create_completion(text, id_)
-        gptresponse, tokens_used = ChatGPTProxy.fetch_response(completion)
+        gptresponse, tokens_used = self.__class__.fetch_response(completion)
         chunk = {"role": "assistant", "content": gptresponse}
         self.chats[id_].append(chunk)
         return gptresponse + f"\n\n used tokens: {tokens_used}"
 
+    def proxy_streamed(self, generator, id_):
+        done = False
+        stream = str()
+        while not done:
+            partial = next(generator)
+            content, finished = self.__class__.fetch_streamed(partial)
+            stream += content
+            yield content
+            done = bool(finished)
+        chunk = {"role": "assistant", "content": stream}
+        self.chats[id_].append(chunk)
+
 
 class GPTbot(telebot.TeleBot):
-    def __init__(self, token, parse_mode=None):
+    def __init__(self, token, streamed=True, parse_mode=None):
         super().__init__(token, parse_mode=parse_mode)
+        self.streamed = streamed
+        self.func_handler = {
+            self.handle_chat_status: ["stopchat", "startchat"],
+            self.starter: ["start", "help"],
+            self.clear_history: ["clearhistory", ],
+            self.set_mode: ["changemode", ],
+            self.dismiss: lambda m: self.chat_stopped,
+            self.answer: lambda m: not self.streamed and not self.chat_stopped,
+            self.answer_dynamic: lambda m: self.streamed and not self.chat_stopped,
+        }
         self.decorate()
         self.proxy = ChatGPTProxy(MODEL, OPENAI_API_KEY)
+        self.chat_stopped = False
+
+    def change_mode(self):
+        new_mode = not self.streamed
+        self.streamed = new_mode
 
     @staticmethod
     def format_response(response):
@@ -69,51 +105,80 @@ class GPTbot(telebot.TeleBot):
         return response
 
     def handle_chat_status(self, message):
-        global STOPCHAT
         if message.text == "/stopchat":
-            STOPCHAT = True
+            self.chat_stopped = True
             self.send_message(
-                message.chat.id, f"⚠️~The chat with ChatGPT is now suspended for @{message.from_user.username}\\.~⚠️", parse_mode="MarkdownV2")
+                message.chat.id, f"⚠️~@{message.from_user.username} suspended the chat with ChatGPT\\.~⚠️", parse_mode="MarkdownV2")
         else:
             self.send_message(
-                message.chat.id, f"The chat with ChatGPT is now continued for @{message.from_user.username} ✔️")
-            STOPCHAT = False
+                message.chat.id, f"The chat with ChatGPT is now continued ✔️")
+            self.chat_stopped = False
 
-    def send_welcome(self, message):
-        greeting = self.proxy.proxy_answer(
-            f"Hello, I am {message.from_user.username}! Please state your purpose and who wrote this telegram bot?", message.from_user.id)
-        self.reply_to(message, greeting)
+    def starter(self, message):
+        if self.streamed:
+            self.answer_dynamic(message)
+        else:
+            self.answer(message)
+
+    def dismiss(self, message):
+        self.reply_to(
+            message, "~chat with ChatGPT is suspended right now\\.~", parse_mode="MarkdownV2")
 
     def answer(self, message):
-        if STOPCHAT:
-            self.reply_to(
-                message, "~chat with ChatGPT is suspended right now\\.~", parse_mode="MarkdownV2")
-            return
         self.send_chat_action(message.chat.id, "typing")
-        reply = self.proxy.proxy_answer(message.text, message.from_user.id)
+        answer = self.proxy.proxy_answer(
+            message.text, message.from_user.id)
+        self.reply_to(message, answer)
 
-        try:
-            self.reply_to(message, GPTbot.format_response(
-                reply), parse_mode="MarkdownV2")
-        except:
-            self.reply_to(message, reply, parse_mode=None)
+    def answer_dynamic(self, message):
+        self.send_chat_action(message.chat.id, "typing")
+        completion = self.proxy.create_completion(
+            message.text, message.from_user.id, streamed=True)
+        content_gen = self.proxy.proxy_streamed(
+            completion, message.from_user.id)
+        partial_content = str()
+        while not partial_content:
+            partial_content = next(content_gen)
+
+        answer = partial_content
+        dynamic = self.reply_to(message, answer)
+        while True:
+            try:
+                for _ in range(5):
+                    partial_content = next(content_gen)
+                    answer += partial_content
+                # trying not to abuse the telegram api...
+                time.sleep(0.01)
+                try:
+                    self.edit_message_text(answer, message.chat.id, dynamic.id)
+                except KeyboardInterrupt:
+                    print("[DEBUG] failed to edit")
+                    continue
+            except StopIteration:
+                self.edit_message_text(answer, message.chat.id, dynamic.id)
+                return
 
     def clear_history(self, message):
         self.proxy.chats[message.from_user.id].clear()
-        self.send_message(message.chat.id, f"@{message.from_user.username}'s history has been cleared 🗑️")
+        self.send_message(
+            message.chat.id, f"@{message.from_user.username}'s history has been cleared 🗑️")
+
+    def set_mode(self, message):
+        self.change_mode()
+        mode = "streamed" if self.streamed else "static"
+        self.send_message(message.chat.id, f"⚠️ Mode is now {mode} ⚠️")
 
     def decorate(self):
-        self.clear_history = self.message_handler(
-            commands=["clearhistory",])(self.clear_history)
-        self.handle_chat_status = self.message_handler(
-            commands=valid_commands)(self.handle_chat_status)
-        self.send_welcome = self.message_handler(
-            commands=["start", "help"])(self.send_welcome)
-        self.answer = self.message_handler(func=lambda m: True)(self.answer)
+        for func in self.func_handler:
+            rhandler = self.func_handler[func]
+            if type(rhandler) is list:
+                func = self.message_handler(commands=rhandler)(func)
+            else:
+                func = self.message_handler(func=rhandler)(func)
 
 
 if __name__ == "__main__":
-    bot = GPTbot(TOKEN, parse_mode=None)
+    bot = GPTbot(TOKEN, parse_mode=None, streamed=True)
     try:
         bot.infinity_polling()
     except:
